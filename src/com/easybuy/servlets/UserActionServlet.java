0b50cd7eadcfcb4ab885388789e6f3762e6b0daf
WenHaiGo <2499039129@qq.com>
2018-02-08 22:54:17 +0800

    问题出在了如何获取当前登录用户的ID？

---
+++ src/com/easybuy/servlets/UserActionServlet.java
@@ -0,0 +1,55 @@
+package com.easybuy.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserActionServlet
+ */
+@WebServlet("/UserActionServlet")
+public class UserActionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserActionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		String param = request.getParameter("param");
+		if(param!=null&&param.equals("cart"))
+		{
+			
+		}
+		
+		
+	
+	}
+
+	//得到购物车所有的商品
+	
+	void getAllCartProduct(HttpServletRequest request, HttpServletResponse response)
+	{
+		
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
