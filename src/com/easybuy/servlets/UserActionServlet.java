e93776c23ee8cfbb6ca7e75a3e193519f8d8fb6c
WenHaiGo <2499039129@qq.com>
2018-02-26 11:49:18 +0800

    将添加操作抽象为方法

--- src/com/easybuy/servlets/UserActionServlet.java
+++ src/com/easybuy/servlets/UserActionServlet.java
@@ -45,14 +45,14 @@ public class UserActionServlet extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		// 先判断是否登录。
 		HttpSession session = request.getSession();
-		String EPUId = (String) session.getAttribute("userName");
-		System.out.println("当前用户是当前用户是当前用户是当前用户是" + EPUId);
+		String EUId = (String) session.getAttribute("userName");
+		System.out.println("当前用户是当前用户是当前用户是当前用户是" + EUId);
 		// clean code 里面说尽量不要做这种判断
-		if (EPUId != null) {
+		if (EUId != null) {
 			// 登录了之后再次判断具体哪一种请求
 			String param = request.getParameter("param");
 			if (param != null && param.equals("cart")) {
-				viewAllCartProduct(request, response, EPUId);
+				viewAllCartProduct(request, response, EUId);
 			}
 
 			if (param != null && param.equals("cartDele")) {
@@ -60,27 +60,13 @@ public class UserActionServlet extends HttpServlet {
 			}
 			// 添加到购物车
 			if (param != null && param.equals("cartAdd")) {
-				// 添加的时候应该首先询问是否有该商品，若有则在数量上增加一 如果没有就新建一个商品
-				int EPId = Integer.parseInt(request.getParameter("EPId"));
-				System.out.println("shangpibainhao shi "+EPId);
-				boolean isSave = eps.cartSave(EPUId, EPId, 1);
-				// 这里不会做弹出窗口，直接输出了
-				if (isSave) {
-					PrintWriter pw = response.getWriter();
-					pw.write("添加成功");
-				}
-
-				else {
-					PrintWriter pw = response.getWriter();
-					pw.write("添加失败");
-				}
-
+				addCartProductByPid(request, response, EUId);
 			}
 		}
 		// 如果没有登录
 		else {
 			try {
-				request.setAttribute("fromCart", "已经登录了");
+				request.setAttribute("notLogin", "未登录");
 				request.getRequestDispatcher("login.jsp").forward(request, response);
 			} catch (IOException | ServletException e) {
 				// TODO Auto-generated catch block
@@ -88,6 +74,22 @@ public class UserActionServlet extends HttpServlet {
 			}
 		}
 
+	}
+
+	void addCartProductByPid(HttpServletRequest request, HttpServletResponse response, String EUId) throws IOException {
+		// 添加的时候应该首先询问是否有该商品，若有则在数量上增加一 如果没有就新建一个商品
+		int EPId = Integer.parseInt(request.getParameter("EPId"));
+		boolean isSave = eps.cartSave(EUId, EPId, 1);
+		// 这里不会做弹出窗口，直接输出了
+		if (isSave) {
+			PrintWriter pw = response.getWriter();
+			pw.write("添加成功");
+		}
+
+		else {
+			PrintWriter pw = response.getWriter();
+			pw.write("添加失败");
+		}
 	}
 
 	void deleCartProductByPid(HttpServletRequest request, HttpServletResponse response) {
@@ -98,15 +100,12 @@ public class UserActionServlet extends HttpServlet {
 		if (isDelete) {
 			try {
 
-				// 感觉删除商品应该使用ajax
 				response.sendRedirect("UserActionServlet?param=cart");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-
-		else {
+		} else {
 			PrintWriter out;
 			try {
 				out = response.getWriter();
@@ -120,10 +119,9 @@ public class UserActionServlet extends HttpServlet {
 
 	}
 	// 得到购物车所有的商品
-
-	void viewAllCartProduct(HttpServletRequest request, HttpServletResponse response, String EPUId) {
-		List<EProduct> cartProductList = eps.getAllCartProduct(EPUId);
-		List<ECartProduct> cartProductInfo = eps.getCartProductInfo(EPUId);
+	void viewAllCartProduct(HttpServletRequest request, HttpServletResponse response, String EUId) {
+		List<EProduct> cartProductList = eps.getAllCartProduct(EUId);
+		List<ECartProduct> cartProductInfo = eps.getCartProductInfo(EUId);
 		request.setAttribute("cartProductInfo", cartProductInfo);
 		request.setAttribute("cartProductList", cartProductList);
 		try {
